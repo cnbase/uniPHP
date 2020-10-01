66f4b1f5892b76215e2b9eaaddd3d0d82da8592e
Qbit <i@qnb.cm>
2020-10-01 22:16:57 +0800

    修改入口文件index.php的demo代码

--- www/index.php
+++ www/index.php
@@ -1,13 +1,12 @@
 <?php
 include '../uniPHP.php';
 
-$uniPHP = new uniPHP([
-    'entryFile' =>  'index.php',
-    'ROOT_DIR'  =>  __DIR__.'/../',
-    'WEB_DIR'   =>  __DIR__,
-    'APP_DIR'   =>  __DIR__.'/../app',
-    'CONF_DIR'  =>  __DIR__.'/../config',
-    'ROUTE_DIR' =>  __DIR__.'/../route',
-    'MODULE_NAME'   =>  'index',
-]);
-$uniPHP->onBeforeCreate(function (){echo 'onBeforeCreate<br>';})->onCreated(function (){echo '<br>onCreated';})->run();
+uniPHP::instance([
+        'entryFile' =>  'index.php',
+        'ROOT_DIR'  =>  __DIR__.'/../',
+        'WEB_DIR'   =>  __DIR__,
+        'APP_DIR'   =>  __DIR__.'/../app',
+        'CONF_DIR'  =>  __DIR__.'/../config',
+        'ROUTE_DIR' =>  __DIR__.'/../route',
+        'MODULE_NAME'   =>  'index',
+    ])->onBeforeCreate(function (){echo 'onBeforeCreate<br>';})->onCreated(function (){echo '<br>onCreated';})->run();
